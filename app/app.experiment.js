65ec22419c41b60be9054811d27f5ac5a1575d83
al-gomeisa <algomeisa@gmail.com>
2018-07-25 06:04:30 +0200

    step4 remove commented lines

--- app/app.experiment.js
+++ app/app.experiment.js
@@ -2,29 +2,3 @@
 
 // Define the `itemsApp` module
 angular.module('itemsApp', []);
-
-/*
-// Define the `itemsApp` module
-var itemsApp = angular.module('itemsApp', []);
-
-// Define the `ExpListController` controller on the `itemsApp` module
-itemsApp.controller('ExpListController', function ExpListController($scope) {
-    $scope.title = 'Doing ExpListController';
-    $scope.items = [
-        {
-            name: 'Item1',
-            description: 'Item1 being the first one.'
-        },
-        {
-            name: 'Nexus S',
-            description: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            description: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            description: 'The Next, Next Generation tablet.'
-        }
-    ];
-});
-*/
