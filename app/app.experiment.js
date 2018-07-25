8c78968de3c914c2d89e37f49cd472f8210d1470
al-gomeisa <algomeisa@gmail.com>
2018-07-25 04:39:21 +0200

    including texperiment js

---
+++ app/app.experiment.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('ExpListController', function ExpListController($scope) {
+  $scope.items = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
