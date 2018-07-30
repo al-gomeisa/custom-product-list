fcbbbbfb145b7bafe34190b37f2762d3b02cf6f9
Daisuke Hiraoka <dhiraoka@statusgreen.de>
2018-07-30 11:30:19 +0200

    adjusting popover window settings

--- app/app.animations.js
+++ app/app.animations.js
@@ -2,7 +2,7 @@
 
 angular.
   module('itemsApp').
-  animation('.item', function itemAnimationFactory() {
+  animation('.itemTEST', function itemAnimationFactory() {
     return {
       addClass: animateIn,
       removeClass: animateOut
@@ -26,6 +26,63 @@ angular.
             
             top: 0,
             left:0,
+            zIndex: 200
+          }, 500, done);
+
+      return function animateInEnd(wasCanceled) {
+       // console.log('animateIN, wasCanceled?', wasCanceled);
+        if (wasCanceled) element.stop();
+      };
+    }
+
+    function animateOut(element, className, done) {
+     // if (className !== 'selected') return;
+
+      element/*.css({
+        
+        position: 'absolute',
+        top: 0,
+        left: 0
+        
+      })*/.animate({
+        opacity: 0,
+        zIndex: 0
+        //top: 500
+      }, 300, done);
+
+      return function animateOutEnd(wasCanceled) {
+        //console.log('animateOUT, wasCanceled?', wasCanceled);
+        if (wasCanceled) element.stop();
+      };
+    }
+  });
+
+angular.
+  module('itemsApp').
+  animation('.item-details', function itemAnimationFactory() {
+    return {
+      addClass: animateIn,
+      removeClass: animateOut
+    };
+
+    function animateIn(element, className, done) {
+          if (className !== 'selected') return;
+
+          element.css({
+            display: 'block',
+         //   opacity: .8,
+        //    position: 'absolute',
+            width: 0,
+            height: 0,
+        //    top: 200,
+        //    left: 200
+          }).animate({
+            opacity: .8,
+            width: 500, // 200,
+            height: 500, //200,
+            
+          //  top: 0,
+          //  left:0,
             zIndex: 200
           }, 500, done);
 
