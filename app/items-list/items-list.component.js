d48b48ed7b969934834e7c9743e49c004e2b4131
Daisuke Hiraoka <dhiraoka@statusgreen.de>
2018-07-30 17:09:36 +0200

    adjust tooltips, buttons on popup window

--- app/items-list/items-list.component.js
+++ app/items-list/items-list.component.js
@@ -7,6 +7,7 @@ angular.
         templateUrl: 'items-list/items-list.template.html',
         controller: ['Item',
             function ExpListController(Item) {
+               // $('.item-list-item').tooltip();
                 var self = this;
                 var orirginalItemsLength;
                 self.items = Item.query(function (result) {
